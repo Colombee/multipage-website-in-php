300d2ff9c9e1e06bd28d08b6f841589acc5c8937
Colombe <c.schh@outlook.com>
2018-07-31 11:00:25 +0200

    email sanitization

--- contact.php
+++ contact.php
@@ -2,8 +2,33 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-    mysqli_real_escape_string ("localhost", "root");
-    $gender = $_POST['gender'];
+    if(@$_POST['button-submit']){
+        $gender = $_POST['gender'];
+
+        $email = $_POST['email'];
+        $san_email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $val_email = filter_var($san_email, FILTER_VALIDATE_EMAIL);
+
+    };
+    function gender(){
+        global $gender;
+        if(@$gender !== false) {
+            return $gender;
+        } 
+        else {
+            return "veuillez indiquer votre genre";
+        }
+    };
+    function val_email(){
+        global $val_email;
+        if(@$val_email !== false) {
+            return $val_email;
+        } 
+        else {
+            return "veuillez indiquer une adresse email valide";
+        }
+    };
+    
 ?>
 
 <!DOCTYPE html>
@@ -22,13 +47,15 @@
     <form method="post" action="contact.php">
         <input type="radio" name="gender" value="monsieur"> Mr 
         <input type="radio" name="gender" value="madame"> Mme
-        <input type="radio" name="gender" value="mademoiselle"> Mlle <br>
+        <input type="radio" name="gender" value="mademoiselle"> Mlle
+        <p style="color:red"><?php echo gender() ?><p>
         Nom :<br>
         <input type="text" name="lastname"><br>
         Prénom :<br>
         <input type="text" name="firstname"><br>
         Email :<br>
-        <input type="text" name="email"><br>
+        <input type="email" name="email">
+        <p style="color:red"><?php echo val_email() ?><p>
         Objet :<br>
         <select name="object-list" form="objects">
             <option value="restaurant">Restaurant</option>
@@ -37,18 +64,15 @@
             <option value="other">Autre</option>
         </select> <br>
         Photo illustrant vos propos : <br>
-        <input type="file" name="button-file">Fichier image</input>
+        <input type="file" name="button-file">
         <br>
         Message : <br>
         <textarea id="textmessage" name="message" style="height:200px"></textarea><br>
         Format de réponse : <br>
         <input type="radio" name="txt" value="html-txt"> HTML
         <input type="radio" name="txt" value="texte-txt"> Texte <br>
-        <input type="submit" value="Envoyer">
+        <input type="submit" name="button-submit" value="Envoyer">
     </form> 
-    <br>
-
-    <?php echo $gender ?>
     <br>
     
 </body>
