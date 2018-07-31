697e57544d16b9555f789620b3482ad223f8f33a
Geoffrey <geoffrey.poelmans@gmail.com>
2018-07-31 10:38:52 +0200

    legere modif footer

--- footer.php
+++ footer.php
@@ -6,5 +6,6 @@
     <img src="images/fse3.jpg" class="fse3" alt="fse3">
     <img src="images/orbem.jpg" alt="orbem">
     <img src="images/ri.gif" class="ri" alt="ri">
+    <p>Copyright © 4 ASBL FOR.E.T, Bd de la 2ème armée britannique 27-1190 Forest Bruxelles</p>
   </div>
 </footer>
