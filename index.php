06573edc6bdaf28e5a29dc68ad46593442331fba
Geoffrey <geoffrey.poelmans@gmail.com>
2018-07-31 09:13:52 +0200

    header footer menu

---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+    <title>FORET asbl</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img class="logo" src="images/logo.png" href="#" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Formations</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="row main">
+        <div class="col-5 contenu offset-2">
+          <p>Depuis janvier 1998, l'asbl FOR.E.T.(FORmation Emploi Tremplin) est reconnue comme Atelier de Formation par le Travail par la COCOF, le FSE, l'IBFFP et l'ORBEM.<p>
+          <p>Elle a pour objet de créer et de gérer des actions visant à former et à qualifier prioritairement des jeunes forestois sans emploi et peu scolarisés.</p>
+          <p>Notre asbl est membre de la FEBISP (Fédération Bruxelloise des organismes d'Insertion Socio-professionnelle).</p>
+          <p>Poursuivant une mission à finalité sociale, l’association développe et coordonne des activités de formation professionnelle et d’économie sociale dans le secteur de la restauration et de l’hôtellerie, en faveur d’adultes sans emploi, peu qualifiés et éloignés du marché du travail et ce dans le respect et la mise en avant de la diversité propre à notre société.</p>
+          <p>Soucieux également de notre environnement, nous avons décidé d’utiliser des produits issus de l’agriculture locale e/ou raisonnée pour notre projet d’épicerie durable/traiteur/table d’hôtes qui axe sa production avec une plus grande place pour le « bien manger », de façon saine, respectueuse des cycles naturels des produits et des saisons.</p>
+        </div>
+        <div class="col-3 mr-auto menu">
+          <h5><p>MENU <i class="fas fa-utensils fa-xs"></i> </p></h5>
+          <ul>
+            <li>
+              <h6>Potage</h6>
+              <ul>
+                <li><p>Soupe du jour</p></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Entrées</h6>
+              <ul>
+                <li><p>Pizza</p></li>
+                <li><p>Rillette de lapin au romarin</p></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Plats</h6>
+              <ul>
+                <li><p>Cuisse de canard confit, lentilles du Puy, pomme de terre sarladèse</p></li>
+                <li><p>Osso bucco, Penne</p></li>
+                <li><p>Brandade de cabillaud gratiné, jus à la sauge, salade (Vendredi)</p></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Dessert</h6>
+              <ul>
+                <li><p>Profiteroles</p></li>
+              </ul>
+            </li>
+          </ul>
+          <p class="horaireMenu">Le restaurant est ouvert 4 jours par semaine, du Mardi au Vendredi.</p>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div class="partenaires">
+        <img src="images/bxlformat.jpg" alt="bxlformat">
+        <img src="images/cocof.gif" alt="cocof">
+        <img src="images/febisp1.jpg" alt="febisp1">
+        <img src="images/fse3.jpg" class="fse3" alt="fse3">
+        <img src="images/orbem.jpg" alt="orbem">
+        <img src="images/ri.gif" class="ri" alt="ri">
+      </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
