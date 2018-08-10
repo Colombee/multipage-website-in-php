3409c09a357b0ac0d5ee57301f76a0381fc3fb97
Colombe <c.schh@outlook.com>
2018-08-10 15:41:39 +0200

    footer links

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <footer>
   <div class="partenaires">
-    <img src="images/bxlformat2.png" href="http://www.bruxellesformation.be/" class="logos-footer" alt="logo de Bruxelles Formation">
-    <img src="images/cocof2.png" href="http://www.spfb.brussels/" class="logos-footer" alt="logo de la COCOF">
-    <img src="images/febisp2.png" href="https://www.febisp.be/" class="logos-footer" alt="logo de la Fédération Bruxelloise des organismes d'Insertion SocioProfessionnelle et d'Économie Sociale d'Insertion">
-    <img src="images/fse2.png" href="http://www.fse.be/" class="logos-footer" alt="logo du Fonds social européen">
-    <img src="images/orbem.jpg" href="http://www.actiris.be/" class="logos-footer"alt="logo de l'ORBEM">
-    <img src="images/ri.gif" href="http://www.rotary.belux.org/" class="logos-footer" alt="logo du Rotary International">
+    <a href="http://www.bruxellesformation.be/" target="_blank"><img src="images/bxlformat2.png" class="logos-footer" alt="logo de Bruxelles Formation"></a>
+    <a href="http://www.spfb.brussels/" target="_blank"><img src="images/cocof2.png" class="logos-footer" alt="logo de la COCOF"></a>
+    <a href="https://www.febisp.be/" target="_blank"><img src="images/febisp2.png" class="logos-footer" alt="logo de la Fédération Bruxelloise des organismes d'Insertion SocioProfessionnelle et d'Économie Sociale d'Insertion"></a>
+    <a href="http://www.fse.be/" target="_blank"><img src="images/fse2.png" class="logos-footer" alt="logo du Fonds social européen"></a>
+    <a href="http://www.actiris.be/" target="_blank"><img src="images/orbem.jpg" class="logos-footer"alt="logo de l'ORBEM"></a>
+    <a href="http://www.rotary.belux.org/" target="_blank"><img src="images/ri.gif" class="logos-footer" alt="logo du Rotary International"></a>
     <p id="paragrapheCopyright">Copyright © 4 ASBL FOR.E.T, Bd de la 2ème armée britannique 27-1190 Forest Bruxelles</p>
   </div>
 </footer>
